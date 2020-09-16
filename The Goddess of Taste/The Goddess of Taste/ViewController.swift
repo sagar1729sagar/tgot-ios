0446ae3afa0e0b1f8e5ef3166e2e553be4ef6c7a
sagar1729sagar <ssapps.sagar@gmail.com>
2020-09-16 07:13:35 -0400

    Initial commit

---
+++ The Goddess of Taste/The Goddess of Taste/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Goddess of Taste
+//
+//  Created by XCodeClub on 2020-09-16.
+//  Copyright © 2020 ssapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
